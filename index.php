007cb822a10f5dfe02c839c625c226e5290c44e8
GenoelJunior <genoeljunior@gmail.com>
2020-07-13 16:39:04 -0300

    adcionando a validacao de campos do formulario

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de inscrição</title>
+    <meta name="autor" content="">
+    <meta name="description" content="">
+</head>
+<body>
+<p>FORMULARIO PAR AINSCRIÇÃO DE COMPETIDORES</p>
+
+<form action="script.php" method = "post">
+    <p>Seu nome: <input type="text" name= "nome"></p>
+    <p>Sua idade: <input type="text" name= "idade"></p>
+    <p><input type="submit" value="Enviar dados competidor"></p>
+</form>
+    
+</body>
+</html>
